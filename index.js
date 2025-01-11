13c03702398e75c720b989aca86ff6eaf5c5061c
mgmamen <kamenraido46@gmail.com>
2025-01-11 12:32:15 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+    var go_url = 'https://sfl.gl/';
+    var api = '902aa64029563864219148881a5992d5051eab62';
+    var shorten_includ = ["safefileku.com"];
+</script>
+<script src="//safelinku.com/js/web-script.js"></script>
